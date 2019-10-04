9d05580d31cb5886bc64868bbe699af7707bc457
vunderkind <justin.irabor@gmail.com>
2019-10-04 09:31:45 +0100

    Added App.js code to parse localhost API data

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const serverData = "http://localhost:5000/api/players";
 
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: [],
+      user: {},
+    };
+  };
+
+  componentDidMount() {
+    //mounting the API output here
+    axios.get(serverData)
+    .then(res => {
+      this.setState(
+        {
+          user: res.data,
+        }
+        )
+    })
+  }
+
+  render() {
+    const { user, followers } = this.state;
+    return (
+      <div className="App" >
+        <img alt="photo_of_mogwai" src={user.avatar_url} className="Avatar"></img>
+        <h1 key={user.id}>{user.login}'s Super-cool Github Clique!</h1>
+        <div className="Card">
+          <div className="Row">
+        {followers.map(follower => (
+          <>
+          <img className="Image" key={follower.id} src={follower.avatar_url} alt={follower.id}></img>
+          <h2>{follower.login}</h2>
+          <p>My github clearance level is {follower.type}</p>
+          </>
+        ))}
+      </div>
+      </div>
+      </div>
+    );
+  }
+}
 export default App;
