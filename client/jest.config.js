99a3b7fceac7cbc60413fcf9505485344bb00320
vunderkind <justin.irabor@gmail.com>
2019-10-05 23:51:44 +0100

    Added new version of npm to clear jest version clash on react-create-app devdepedency tree

--- client/jest.config.js
+++ client/jest.config.js
@@ -15,7 +15,7 @@ module.exports = {
   // cacheDirectory: "C:\\Users\\mogwai\\AppData\\Local\\Temp\\jest",
 
   // Automatically clear mock calls and instances between every test
-  // clearMocks: false,
+  clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
@@ -24,7 +24,7 @@ module.exports = {
   // collectCoverageFrom: null,
 
   // The directory where Jest should output its coverage files
-  // coverageDirectory: null,
+  coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
--- client/src/App.test.js
+++
@@ -1,8 +0,0 @@
-
-describe('sum function', () => {
-  it('sums two integers', () => {
-    // This is a test block. Note the test runner
-    // is reporting a success. Not so soon!
-    // This test is not done.
-  });
-}); 
